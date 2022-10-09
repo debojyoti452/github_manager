235243a5526351b14c67fbe6bd10b8cba9b23a0f
debojyoti452 <debojyoti452@gmail.com>
2022-10-09 12:44:30 +0530

    common-module-added

---
+++ gitmanagersharedmodule/src/androidMain/kotlin/com/swing/gitmanagersharedmodule/Platform.kt
@@ -0,0 +1,7 @@
+package com.swing.gitmanagersharedmodule
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
